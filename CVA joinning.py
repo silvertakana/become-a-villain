ff18d8eea0be9f95e67a60e692a6b126f827336d
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-05-13 10:32:42 +1200

    Update CVA joinning.py

--- CVA joinning.py
+++ CVA joinning.py
@@ -1,15 +1,17 @@
 import random
 id = round(random.random() * 8999 + 1000)
 name = input("your name: ")
-print("hello ("+name+"), welcome to the Cyber Villain Association")
+print("hello "+name+", welcome to the Cyber Villain Association")
 print("call it CVA for short")
 print("now please enter your age")
-age = input("age: ")
+year_of_birth = int(input("year of birth: "))
+this_year = 2021
+age = this_year - year_of_birth
 
 # print(id)
 if int(age) < 17:
-    print("you are "+age+"? pfft you are too young go home kid")
+    print("you are "+str(age)+"? pfft you are too young, go home kid")
 else:
+    print("you must be "+str(age)+" years old")
     print("congrats you are now officially a member of CVA.")
-
-    print("we will call you "+name+age+"#"+str(id))
+    print("your code name is "+name+str(age)+"#"+str(id))
